2e288116a07c792fef4fb2065868aa7611b06388
GLOBAL\Giovanni.Brogiolo <giovanni.brogiolo>
2019-05-12 12:55:30 +1000

    changes

--- SimpleDockablePanel/Views/ImportedDWGview.xaml.cs
+++ SimpleDockablePanel/Views/ImportedDWGview.xaml.cs
@@ -1,4 +1,5 @@
-﻿using SimpleDockablePanel.Models;
+﻿using Autodesk.Revit.UI;
+using SimpleDockablePanel.Models;
 using System.Collections.Generic;
 using System.Windows.Controls;
 
@@ -17,11 +18,17 @@ namespace SimpleDockablePanel.Views
 
             this.DataContext = new ViewModels.ImportDWGViewModel();
 
+            _DWGHandler = new RevitAddinWPF.Command();
+            _DWGEvent = ExternalEvent.Create(_DWGHandler);
 
+            _DWGEvent.Raise();
         }
 
         private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
         {
+            
+            _DWGEvent.Raise();
+
             stPanel.Children.Clear();
 
             RevitAddinWPF.Views.viewRevitBridge view = new RevitAddinWPF.Views.viewRevitBridge();
@@ -29,6 +36,23 @@ namespace SimpleDockablePanel.Views
             view.DataContext = RevitAddinWPF.Command.vmod;
 
             stPanel.Children.Add(view);
+        }
+
+
+
+        private RevitAddinWPF.Command _DWGHandler;
+        private ExternalEvent _DWGEvent;
+
+        public void DWGeventHandler()
+        {
+            _DWGHandler = new RevitAddinWPF.Command();
+            _DWGEvent = ExternalEvent.Create(_DWGHandler);
+
+        }
+
+        public void DWGRaise(object obj)
+        {
+            _DWGEvent.Raise();
         }
     }
 }
